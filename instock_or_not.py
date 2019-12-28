f62e3e06423a32c2490371e345967ee97df011e4
xalxnder <xalexander51@gmail.com>
2019-12-27 20:40:36 -0500

    Added the get_items function

--- instock_or_not.py
+++ instock_or_not.py
@@ -10,17 +10,22 @@ supreme = requests.get("https://www.supremenewyork.com/shop/all")
 # The Beautiful Soup Object
 soup = BeautifulSoup(supreme.content, 'html.parser')
 all_items = soup.select('.inner-article')
-for item in all_items:
-    link = item.find('a')
-    extension = link.attrs['href']
-    new_supreme = requests.get(f"https://supremenewyork.com{extension}")
-    if item.text != 'sold out':
-        more_soup = BeautifulSoup(new_supreme.content, 'html.parser')
-        actual_item = more_soup.select('#details')
-        for description in actual_item:
-            final_item = description.find('h2').text
-            color = description.find('p').text
-            print(
-                Fore.GREEN + f"Item:{final_item}\n Color: {color}\n https://supremenewyork.com{extension} \n \n")
 
+def get_items():
+    for item in all_items:
+        link = item.find('a')
+        extension = link.attrs['href']
+        new_supreme = requests.get(f"https://supremenewyork.com{extension}")
+        if item.text != 'sold out':
+            more_soup = BeautifulSoup(new_supreme.content, 'html.parser')
+            actual_item = more_soup.select('#details')
+            for description in actual_item:
+                final_item = description.find('h2').text
+                color = description.find('p').text
+                print(
+                    Fore.GREEN + f"Item:{final_item}\n Color: {color}\n https://supremenewyork.com{extension} \n \n")
+
+get_items()
+
+print('done')
     # print(pic.attrs['src'])
